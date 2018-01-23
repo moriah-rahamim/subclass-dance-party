4af7400e6a38fba2c279707cc0ee1414ca36a214
moriah-rahamim <moriahfaye@gmail.com>
2018-01-23 11:05:07 -0800

    Calculate when an OverconfidentDancer is touching another dancer

--- src/Overconfident.js
+++ src/Overconfident.js
@@ -8,10 +8,43 @@ var OverconfidentDancer = function(top, left) {
 OverconfidentDancer.prototype = Object.create(Dancer.prototype);
 OverconfidentDancer.prototype.constructor = OverconfidentDancer;
 
+OverconfidentDancer.prototype.findNeighbors = function() {
+    let confidentWidth = this.$node.css('border-width');
+    confidentWidth = parseInt(confidentWidth.slice(0, -2));
+    let confidentCenter = {
+        top: this.top + (confidentWidth/2),
+        left: this.left + (confidentWidth/2)
+    };
+    window.dancers.forEach(ele => {
+        if (!(ele instanceof OverconfidentDancer)) {
+            let dancerWidth = ele.$node.css('border-width');
+            dancerWidth = parseInt(dancerWidth.slice(0, -2));
+            let dancerCenter = {
+                top: ele.top + (dancerWidth/2),
+                left: ele.left + (dancerWidth/2),
+            };
+            let distanceBetween = Math.sqrt(
+                Math.pow(confidentCenter.top - dancerCenter.top, 2) +
+                Math.pow(confidentCenter.left - dancerCenter.left, 2)
+            );
+            if (distanceBetween < (dancerWidth + confidentWidth)){
+                // remove the other dancer
+            }
+
+        }
+    })
+}
 OverconfidentDancer.prototype.step = function() {
     Dancer.prototype.step.call(this);
     let rand = Math.random() * 100;
 
     this.$node.css("transition", `border ${this.timeBetweenSteps}ms`);
     this.$node.css("border-width", `${rand}px`);
+
 };
+
+
+//{left: 200, top: 800}
+
+//Divide left and top by 2 to find the center.
+//If distance between centers is less than radius of A + radius of B
