adcfb2816eba3d3d1487566ca85d5c95f32c5cb6
moriah-rahamim <moriahfaye@gmail.com>
2018-01-22 19:47:48 -0800

    Added tests for rainbowDancer

--- spec/rainbowDancerSpec.js
+++ spec/rainbowDancerSpec.js
@@ -6,17 +6,23 @@ describe('RainbowDancer', function() {
   beforeEach(function() {
     clock = sinon.useFakeTimers();
     rainbowDancer = new RainbowDancer(10, 20, timeBetweenSteps);
+    rainbowDancer.step();
+    // ^^ delete me if weird shit starts happening
   });
 
   it('should change colors', function() {
     // capture the current css color
     let oldColor = rainbowDancer.$node.css('border-color');
-    // wait 101 milliseconds
-    setTimeout(function() {
-      let newColor = rainbowDancer.$node.css('border-color');
-      expect(newColor).to.not.equal(oldColor);
-    }, 101);
+    // wait 100 milliseconds
+    clock.tick(timeBetweenSteps);
     // capture it again
     // it should be a different color
+    let newColor = rainbowDancer.$node.css('border-color');
+    expect(newColor).to.not.equal(oldColor);
   });
+  
+  it('should have a jQuery node', function() {
+    expect(rainbowDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
 });
