609e5b018bc00391ed92040e577991ed2a8a4d66
moriah-rahamim <moriahfaye@gmail.com>
2018-01-23 10:34:44 -0800

    Line up them dancers

--- src/init.js
+++ src/init.js
@@ -2,21 +2,7 @@ $(document).ready(function() {
   window.dancers = [];
 
   $('.addDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
-
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-    // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
@@ -27,7 +13,24 @@ $(document).ready(function() {
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
-    dancer.step()
+    dancer.step();
+    window.dancers.push(dancer);
   });
+
+  $('.lineUpButton').on('click', function(event) {
+      let heightOfWindow = $('body').height();
+      let spacing = heightOfWindow/(window.dancers.length + 1);
+      let currentHeight = spacing;
+      window.dancers.forEach(ele => {
+          ele.left = 0;
+          // ele.$node.css('transition','left 1s');
+          ele.$node.css('left','0px');
+          ele.top = currentHeight;
+          ele.$node.css('transition','top 1s');
+          ele.$node.css('top', currentHeight + 'px');
+          currentHeight += spacing;
+      })
+  })
+
 });
 
