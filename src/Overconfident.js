869689104ffb39cbcfce6a5b76b3ec39c803c8e7
moriah-rahamim <moriahfaye@gmail.com>
2018-01-23 12:04:34 -0800

    Overconfident Dancers absorb neighboring dancers

--- src/Overconfident.js
+++ src/Overconfident.js
@@ -8,15 +8,16 @@ var OverconfidentDancer = function(top, left) {
 OverconfidentDancer.prototype = Object.create(Dancer.prototype);
 OverconfidentDancer.prototype.constructor = OverconfidentDancer;
 
-OverconfidentDancer.prototype.findNeighbors = function() {
+OverconfidentDancer.prototype.absorbNeighbors = function() {
     let confidentWidth = this.$node.css('border-width');
     confidentWidth = parseInt(confidentWidth.slice(0, -2));
     let confidentCenter = {
         top: this.top + (confidentWidth/2),
         left: this.left + (confidentWidth/2)
     };
-    window.dancers.forEach(ele => {
+    window.dancers.forEach((ele, idx) => {
         if (!(ele instanceof OverconfidentDancer)) {
+            console.log(ele);
             let dancerWidth = ele.$node.css('border-width');
             dancerWidth = parseInt(dancerWidth.slice(0, -2));
             let dancerCenter = {
@@ -29,10 +30,12 @@ OverconfidentDancer.prototype.findNeighbors = function() {
             );
             if (distanceBetween < (dancerWidth + confidentWidth)){
                 // remove the other dancer
+                ele.$node.hide();
+                window.dancers.splice(idx, 1);
             }
 
         }
-    })
+    });
 }
 OverconfidentDancer.prototype.step = function() {
     Dancer.prototype.step.call(this);
@@ -41,6 +44,7 @@ OverconfidentDancer.prototype.step = function() {
     this.$node.css("transition", `border ${this.timeBetweenSteps}ms`);
     this.$node.css("border-width", `${rand}px`);
 
+    this.absorbNeighbors();
 };
 
 
